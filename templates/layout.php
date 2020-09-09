95f083613e2e9c0ba3a4c8d84876fa08f84ba9a4
Dinar Gabdrakhmanov <fetgea@gmail.com>
2020-09-09 19:37:37 +0300

    Добавил корректные ссылки на страницы

--- templates/layout.php
+++ templates/layout.php
@@ -19,7 +19,7 @@
             <input type="search" name="search" placeholder="Поиск лота">
             <input class="main-header__search-btn" type="submit" name="find" value="Найти">
         </form>
-        <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
+        <a class="main-header__add-lot button" href="add.php">Добавить лот</a>
 
         <nav class="user-menu">
 		<?php if ($is_auth == 1):?>
@@ -31,7 +31,7 @@
 		<?php else : ?>
 			<ul class="user-menu__list">
 				<li class="user-menu__item">
-					<a href="#">Регистрация</a>
+					<a href="signup.php">Регистрация</a>
 				</li>
 				<li class="user-menu__item">
 					<a href="#">Вход</a>
