1fd7e556f26e836297b965660bc5cfec31a13341
Dinar Gabdrakhmanov <fetgea@gmail.com>
2020-09-15 19:55:17 +0300

    Модуль 7 Задание 2 Аутентифицируемся на сайте

--- index.php
+++ index.php
@@ -1,46 +1,52 @@
 <?php
 require './helpers.php';
+session_start();
+$is_auth = $_SESSION['user_logged_in'] ?? false;
+$user_name = $_SESSION['user_name'] ?? "";
 
-$is_auth = rand(0, 1);
-
-$user_name = 'Dinar'; // укажите здесь ваше имя
-
-$connection = mysqli_connect('localhost','root','root','yeti_cave_db');
+$connection = mysqli_connect('localhost', 'root', 'root', 'yeti_cave_db');
 
 $items = [];
 $categories = get_categories();
 
 if (!$connection) {
-    
+
     print('Ошибка подключения к БД: ' . mysqli_connect_error());
-    
+
 } else {
 
     mysqli_set_charset($connection, "utf8");
-    $sql_items = "SELECT lots.lot_id, lot_name AS 'name', cat.name AS 'category', img_url, starting_price AS 'price',  MAX(bid_price) AS 'bid_price' , date_end AS 'expiration_date' 
-        FROM lots 
+    $sql_items = "SELECT lots.lot_id, lot_name AS 'name', cat.name AS 'category', img_url, starting_price AS 'price',  MAX(bid_price) AS 'bid_price' , date_end AS 'expiration_date'
+        FROM lots
         INNER JOIN categories AS cat ON lots.category_id = cat.category_id
         LEFT JOIN bids ON lots.lot_id = bids.lot_id
         WHERE date_end > CURDATE()
         GROUP BY lots.lot_id
         ORDER BY date_created DESC";
     $items_res = mysqli_query($connection, $sql_items);
-    
-    if ($items_res) { 
-    
+
+    if ($items_res) {
+
         $items = mysqli_fetch_all($items_res, MYSQLI_ASSOC);
-        
+
     } else {
-    
+
         print('Ошибка запроса: ' . mysqli_error($connection));
-    
+
     }
-    
+
 }
 
 
-$page_content = include_template('/main.php', ['categories'=>$categories, 'items'=>$items ]);
+$page_content = include_template('/main.php', ['categories' => $categories, 'items' => $items]);
 
-$layout_content = include_template('/layout.php', ['content' =>$page_content, 'title' => 'Главная' ,'categories' => $categories, 'is_auth' => $is_auth, 'user_name' => $user_name, 'homepage' => true ]); 
+$layout_content = include_template('/layout.php', [
+    'content' => $page_content,
+    'title' => 'Главная',
+    'categories' => $categories,
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+    'homepage' => true
+]);
 
 print($layout_content);
--- templates/sign-up.php
+++ templates/sign-up.php
@@ -1,30 +1,31 @@
-    <form class="form container <?php if (!empty($errors)) : ?>
+<form class="form container <?php if (!empty($errors)) : ?>
     form--invalid
     <?php endif; ?>
     " action="signup.php" method="post" autocomplete="off"> <!-- form--invalid -->
-      <h2>Регистрация нового аккаунта</h2>
-      <div class="form__item <?= isset($errors['email']) ? "form__item--invalid" : "" ?>"> <!-- form__item--invalid -->
+    <h2>Регистрация нового аккаунта</h2>
+    <div class="form__item <?= isset($errors['email']) ? "form__item--invalid" : "" ?>"> <!-- form__item--invalid -->
         <label for="email">E-mail <sup>*</sup></label>
         <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?= get_post_val('email') ?>">
         <span class="form__error"><?= $errors['email'] ?? "" ?></span>
-      </div>
-      <div class="form__item <?= isset($errors['password']) ? "form__item--invalid" : "" ?>">
+    </div>
+    <div class="form__item <?= isset($errors['password']) ? "form__item--invalid" : "" ?>">
         <label for="password">Пароль <sup>*</sup></label>
-        <input id="password" type="password" name="password" placeholder="Введите пароль" value="<?= get_post_val('password') ?>">
+        <input id="password" type="password" name="password" placeholder="Введите пароль"
+               value="<?= get_post_val('password') ?>">
         <span class="form__error"><?= $errors['password'] ?? "" ?></span>
-      </div>
-      <div class="form__item <?= isset($errors['name']) ? "form__item--invalid" : "" ?>">
+    </div>
+    <div class="form__item <?= isset($errors['name']) ? "form__item--invalid" : "" ?>">
         <label for="name">Имя <sup>*</sup></label>
         <input id="name" type="text" name="name" placeholder="Введите имя" value="<?= get_post_val('name') ?>">
         <span class="form__error"><?= $errors['name'] ?? "" ?></span>
-      </div>
-      <div class="form__item <?= isset($errors['message']) ? "form__item--invalid" : "" ?>">
+    </div>
+    <div class="form__item <?= isset($errors['message']) ? "form__item--invalid" : "" ?>">
         <label for="message">Контактные данные <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите как с вами связаться"><?= get_post_val('message') ?></textarea>
+        <textarea id="message" name="message"
+                  placeholder="Напишите как с вами связаться"><?= get_post_val('message') ?></textarea>
         <span class="form__error"><?= $errors['message'] ?? "" ?></span>
-      </div>
-      <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
-      <button type="submit" class="button">Зарегистрироваться</button>
-      <a class="text-link" href="#">Уже есть аккаунт</a>
-    </form>
- 
+    </div>
+    <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+    <button type="submit" class="button">Зарегистрироваться</button>
+    <a class="text-link" href="login.php">Уже есть аккаунт</a>
+</form>
